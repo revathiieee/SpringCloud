a4c931fd78e6c64a20aa3f8c16734c42fb6b43a1
techragesh <techragesh@gmail.com>
2019-01-16 22:15:10 +0100

    Committing config properties file

--- config-client/src/main/java/com/spring/cloud/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/spring/cloud/configclient/ConfigClientApplication.java
@@ -2,9 +2,7 @@ package com.spring.cloud.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-@RefreshScope
 @SpringBootApplication
 public class ConfigClientApplication {
 
--- config-client/src/main/java/com/spring/cloud/configclient/MessageResource.java
+++ config-client/src/main/java/com/spring/cloud/configclient/MessageResource.java
@@ -1,15 +1,17 @@
 package com.spring.cloud.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/rest")
 public class MessageResource {
 
-    @Value("${message :Default Hello}")
+    @Value("${message: Config server is down. Sorry for inconvenience..!}")
     private String message;
 
     @GetMapping("/message")
